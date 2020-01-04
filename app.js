19ece0159940f573d036f36666fdaad13ac27431
MartinGwon <anatomy1545@gmail.com>
2020-01-04 18:14:26 +0900

    finishing js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const fill = document.getElementById("jsMode");
+const save = document.getElementById("jsSave");
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
 
-ctx.strokeStle = "black";
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
+ctx.fillStyle = "black";
 
 let painting = false;
+let filling = false;
 
 function handleMouseMove(event) {
   const x = event.offsetX;
@@ -33,11 +41,58 @@ function startPainting() {
   painting = true;
 }
 
+function handleColorClick(event) {
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = ctx.strokeStyle;
+}
+
+function handleRangeChange(event) {
+  ctx.lineWidth = event.target.valueAsNumber;
+}
+
+function handleFillClick(event) {
+  if (filling === true) {
+    filling = false;
+    fill.innerText = "Fill";
+  } else {
+    filling = true;
+    fill.innerText = "Paint";
+    ctx.fillStyle = ctx.strokeStyle;
+  }
+}
+
+function handleCanvasClick() {
+  if (filling) {
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+  }
+}
+
+function handleRightClick(event) {
+  event.preventDefault();
+}
+
+function handleSave() {
+  const image = canvas.toDataURL();
+  const link = document.createElement("a");
+  link.href = image;
+  link.download = "mapainting.png";
+  link.click();
+}
+
 function init() {
   canvas.addEventListener("mousemove", handleMouseMove);
   canvas.addEventListener("mousedown", startPainting);
   canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
+  canvas.addEventListener("click", handleCanvasClick);
+  canvas.addEventListener("contextmenu", handleRightClick);
+  Array.from(colors).forEach(color =>
+    color.addEventListener("click", handleColorClick)
+  );
+  range.addEventListener("input", handleRangeChange);
+  fill.addEventListener("click", handleFillClick);
+  save.addEventListener("click", handleSave);
 }
 
 init();
